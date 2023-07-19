e4c8de2f52b4f6d656af89649d5aa182a750a085
CodeNoGorD <code.nogord@gmail.com>
2023-07-19 20:04:50 +0200

    fonction reverseTab et ensuite fillData pour remplir les numeros des cases avec une fonction checkMines

--- JS/Game.js
+++ JS/Game.js
@@ -12,6 +12,7 @@ window.addEventListener('load', () => {
     const cols = document.querySelector('#cols');
     const mines = document.querySelector('#mines');
     const btnStart = document.querySelector('#btn-submit');
+    let GAMEOVER = false;
 
     class Game {
         constructor(rows, cols, mines) {
@@ -40,28 +41,51 @@ window.addEventListener('load', () => {
         }
     }
 
+    // EVENT LISTENER DU BOUTTON FORMULAIRE
     btnStart.addEventListener('click', async (e) => {
         e.preventDefault();
+        // CREATION DU JEU
         let game = new Game(rows.value, cols.value, mines.value);
+        // RECUPERATION DES DATAS EN SE CONNECTANT A l'API
         let gameDatas = await game.getDatas(`https://minesweeper.js.apprendre-est.fun/generate_grid.php?
         rows=${rows.value}&cols=${cols.value}&mines=${mines.value}`);
+        //TRANSITION DE LA PAGE AVEC APPARITION DE LA GRILLE
         formArea.classList.add('d-none');
         title.classList.remove('d-none');
         title.classList.add('d-block');
         title.innerText = `Bienvenue ${pseudo.value}`;
         gridArea.classList.remove('d-none');
         gridArea.classList.add('d-block');
-        game.drawArea(gameDatas);
-        console.dir(game);
+        console.dir(gameDatas);
+
+        // INVERSION DU SIGNE 1 DANS LE TABLEAU POUR EVITER LES CONFLITS
+        let reversedDatas = Tools.reverseSign(gameDatas);
+        console.dir(reversedDatas);
+        let savedData = JSON.parse(JSON.stringify(reversedDatas));
+        console.dir(savedData);
+
+        // INSCRIRE LES VALEURS AUTOURS DES BOMBES DANS LE TABLEAU
+        let filledDatas = Tools.fillDatas(savedData, game.rows, game.cols);
+        console.dir(filledDatas);
+        // DESSINER LA GRILLE
+        game.drawArea(filledDatas);
     });
+
+    // EVENT LISTENER DE LA GRILLE
     gridArea.addEventListener('click', (e) => {
-        if (e.target.classList.contains('square-hidden') && e.target.dataset.value != 1) {
+        let count = 0;
+
+        if (e.target.classList.contains('square-hidden') && e.target.dataset.value != -1) {
             e.target.classList.remove('square-hidden');
             e.target.style.background = '#9c9a9a';
+            // console.log(e.target.dataset.rowIndex);
+            // console.log(e.target.dataset.colIndex);
+            e.target.innerText = e.target.dataset.value;
         }
-        if (e.target.dataset.value == 1) {
+        if (e.target.dataset.value == -1) {
             e.target.style.backgroundImage = "url(/public/img/mine2.png)";
+            GAMEOVER = true;
+            console.log('GAMEOVER');
         }
-        // Tools.revealSquare();
     });
 });
--- JS/Tools.js
+++ JS/Tools.js
@@ -4,7 +4,99 @@ const gridArea = document.querySelector('.grid-area');
 export class Tools {
     constructor() {
     }
-    static dataToHTML(gameDatas){
+
+    static reverseSign(arr) {
+        let tab = []
+        arr.forEach((row) => {
+            let line = [];
+            row.forEach((col) => {
+                if (col == 1) {
+                    line.push(-col);
+                } else {
+                    line.push(col);
+                }
+            });
+            tab.push(line);
+        });
+        return tab;
+    }
+
+    static checkMines(arr, row, col, nbRows, nbCols) {
+        let mines = 0;
+        let count = 0;
+        // GESTION DU CENTRE
+        if (arr[row - 1] != undefined && arr[row + 1] != undefined
+            && arr[row][col - 1] != undefined && arr[row][col + 1] != undefined) {
+            arr[row - 1][col - 1] == -1 ? mines++ : '';
+            arr[row - 1][col] == -1 ? mines++ : '';
+            arr[row - 1][col + 1] == -1 ? mines++ : '';
+            arr[row][col - 1] == -1 ? mines++ : '';
+            arr[row][col + 1] == -1 ? mines++ : '';
+            arr[row + 1][col - 1] == -1 ? mines++ : '';
+            arr[row + 1][col] == -1 ? mines++ : '';
+            arr[row + 1][col + 1] == -1 ? mines++ : '';
+        }
+
+        // GESTION DES BORDS
+        // GESTION HAUT
+        if (arr[row - 1] == undefined) {
+            if (arr[row][col - 1] == undefined) {
+                arr[row + 1][col] == -1 ? mines++ : '';
+                arr[row + 1][col + 1] == -1 ? mines++ : '';
+                arr[row][col + 1] == -1 ? mines++ : '';
+            } else if (arr[row][col + 1] == undefined) {
+                arr[row + 1][col] == -1 ? mines++ : '';
+                arr[row + 1][col - 1] == -1 ? mines++ : '';
+                arr[row][col - 1] == -1 ? mines++ : '';
+            } else {
+                arr[row][col - 1] == -1 ? mines++ : '';
+                arr[row][col + 1] == -1 ? mines++ : '';
+                arr[row + 1][col - 1] == -1 ? mines++ : '';
+                arr[row + 1][col] == -1 ? mines++ : '';
+                arr[row + 1][col + 1] == -1 ? mines++ : '';
+            }
+        }
+
+        // GESTION BAS
+        if (arr[row + 1] == undefined) {
+            if (arr[row][col - 1] == undefined) {
+                arr[row - 1][col] == -1 ? mines++ : '';
+                arr[row - 1][col + 1] == -1 ? mines++ : '';
+                arr[row][col + 1] == -1 ? mines++ : '';
+            } else if (arr[row][col + 1] == undefined) {
+                arr[row - 1][col] == -1 ? mines++ : '';
+                arr[row - 1][col - 1] == -1 ? mines++ : '';
+                arr[row][col - 1] == -1 ? mines++ : '';
+            } else {
+                arr[row][col - 1] == -1 ? mines++ : '';
+                arr[row][col + 1] == -1 ? mines++ : '';
+                arr[row - 1][col - 1] == -1 ? mines++ : '';
+                arr[row - 1][col] == -1 ? mines++ : '';
+                arr[row - 1][col + 1] == -1 ? mines++ : '';
+            }
+        }
+
+        return mines;
+    }
+
+    static fillDatas(datas, nbRows, nbCols) {
+
+        datas.forEach((row, rowIndex) => {
+            row.forEach((col, colIndex) => {
+                if (col === 0) {
+                    let nbrMines = Tools.checkMines(datas, rowIndex, colIndex, nbRows, nbCols);
+                    if (nbrMines > 0) {
+                        datas[rowIndex][colIndex] += nbrMines;
+                    } else {
+                        datas[rowIndex][colIndex] = 0;
+                    }
+                }
+            })
+        });
+        return datas;
+    }
+
+    static dataToHTML(gameDatas) {
         gameDatas.forEach((row, rowIndex) => {
             let line = document.createElement('div');
             line.classList.add('d-flex');
@@ -18,9 +110,5 @@ export class Tools {
             });
             gridArea.append(line);
         });
-    }
-    static revealSquare() {
-
-        return 'ca marche';
     }
 }
